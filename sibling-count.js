52c321491859488e7e8777f8bb2d023c0ff0f723
Andrew Hudson <ahudson@gmail.com>
2024-06-11 18:00:38 +0100

    remove lit etc

---
+++ sibling-count.js
@@ -0,0 +1,26 @@
+class SiblingCount extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+	<slot></slot>
+	`;
+  }
+
+  connectedCallback() {
+    const shadow = this.shadowRoot;
+    const slot = shadow.querySelector("slot");
+    const parent = slot.assignedNodes()[1];
+
+    const siblingCount = parent.childElementCount;
+    parent.style.setProperty("--sibling-count", siblingCount);
+
+    const siblings = parent.children;
+    // Loop through all the children and add the custom property sibling-index to each.
+    [...siblings].forEach((sibling, index) => {
+      sibling.style.setProperty("--sibling-index", index);
+    });
+  }
+}
+
+customElements.define("sibling-count", SiblingCount);
